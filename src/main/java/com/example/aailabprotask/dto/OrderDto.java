9e51b6f5e95ac3e7d812527e55a85ca2d2013268
safarovrag <ramin.safarov@kapitalbank.az>
2023-05-17 12:57:11 +0400

    smt

---
+++ src/main/java/com/example/aailabprotask/dto/OrderDto.java
@@ -0,0 +1,8 @@
+package com.example.aailabprotask.dto;
+
+import lombok.Data;
+
+@Data
+public class OrderDto {
+    String name;
+}
---
+++ src/test/java/com/example/aailabprotask/service/CardProfileServiceTest.java
@@ -0,0 +1,15 @@
+package com.example.aailabprotask.service;
+
+import com.example.aailabprotask.dto.OrderDto;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CardProfileServiceTest {
+
+
+    @Test
+    void getCardProfile() {
+    }
+}
